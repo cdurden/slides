f202dcb01126b2fe33b1250ecb83de28651fefaf
Chris Durden <cdurden@gmail.com>
2020-03-24 18:42:55 -0500

    schedule

--- md.js
+++ md.js
@@ -4,6 +4,16 @@ var app = angular.module('md', [
     'slides.services.sockets',
 ]);
 app.controller("MyController", ["$scope", "$location", "$http", function($scope, $location, $http) {
-    $scope.vm = {};
-    $scope.vm.mymarkdown = "# Heading";
+  $scope.markdown = "";
+  md = $location.search().md;
+  $http({
+    method: 'GET',
+    url: "./markdown/"+md+".md"
+  }).then(function success(response) {
+      console.log(response);
+      $scope.markdown = response.data;
+  }, function error(response) {
+  });
+
+
 }]);
