9fb60e92841307a3ecf009c50126a18eb7bcb98e
Chris Durden <cdurden@gmail.com>
2020-03-25 00:53:22 -0500

    ng-transcluding

--- directives/snow-qm-task.js
+++ directives/snow-qm-task.js
@@ -4,7 +4,11 @@ angular.module('slides')
     restrict: 'A',
     require: ['snowQmTask'],
     replace: true,
-    templateUrl: './templates/html_task.html',
+    //templateUrl: './templates/html_task.html',
+    templateUrl: function(element, attrs) {
+        console.log(attrs);
+        return(attrs.id);
+    },
     scope: { collection: '=', task: '=' },
     bindToController: { collection: '@', task: '@' },
     controller: ["$scope","$sce", function ($scope, $sce) {
