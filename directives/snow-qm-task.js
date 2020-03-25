65f1545efbb8459c24a7e3ea67d02c446dc98755
Chris Durden <cdurden@gmail.com>
2020-03-25 02:39:19 -0500

    waiting-to-inject-questions

--- directives/snow-qm-task.js
+++ directives/snow-qm-task.js
@@ -37,7 +37,7 @@ angular.module('slides')
           });
           Sockets.emit("get-snow-qm-task", {'collection': this.collection, 'task': this.task});
         }
-        timer = setTimeout(if(Reveal.isReady()) inject_questions(), 1000); // call every 1000 milliseconds
+        timer = setTimeout(function() { if(Reveal.isReady()) inject_questions()}, 1000); // call every 1000 milliseconds
       }
       this.submit = function (ev) {
           ev.preventDefault(); // prevents page reloading
