2a0b064ebba39dd0a9b26c996f0310102c34b750
Chris Durden <cdurden@gmail.com>
2020-03-25 19:21:33 -0500

    sliding

--- app.js
+++ app.js
@@ -75,6 +75,21 @@ app.config(['$routeProvider', function ($routeProvider) {
             controller: 'MyController'
             });
 }]);
+/*
+  app.directive('contentDirective', function() {
+    return {
+      restrict: 'E',
+      scope: false,
+      link: function(scope, elem, attr) {
+        if (attr.type === 'text/javascript-lazy') {
+          var code = elem.text();
+          var f = new Function(code);
+          f();
+        }
+      }
+    };
+  });
+  */
 app.directive("myInclude", function() {
   return {
     restrict: 'CAE',
@@ -94,21 +109,6 @@ app.directive("myInclude", function() {
     template: "<div ng-include='myInclude||src'></div><div ng-show='loadFailed' ng-transclude/>"
   };
 })
-/*
-  app.directive('script', function() {
-    return {
-      restrict: 'E',
-      scope: false,
-      link: function(scope, elem, attr) {
-        if (attr.type === 'text/template') {
-          var code = elem.text();
-          var f = new Function(code);
-          f();
-        }
-      }
-    };
-  });
-  */
 /*
 app.config([ '$httpProvider' , function ($httpProvider) {
   $httpProvider.defaults.useXDomain = true ;
