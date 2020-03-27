3940ad0ccc3f793e06d02d587ad8cc5172aa5116
Chris Durden <cdurden@gmail.com>
2020-03-26 19:13:44 -0500

    retrying-parent+child-directives

--- app.js
+++ app.js
@@ -135,7 +135,8 @@ app.directive("slidesSection", function() {
   return {
     restrict: 'E',
     //templateUrl: './templates/section.html',
-    template: '<section ng-repeat="slide in section"><html-slide slide="slide"></html-slide></ng-init></section>',
+    template: '<section ng-repeat="slide in section"><html-slide slide="slide"></html-slide></section>',
+    //template: '<section ng-repeat="slide in section"><html-slide slide="slide"></html-slide></ng-init></section>',
     replace: true,
     require: '^slideshow',
     scope: {
